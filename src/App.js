209e9eaff75e75657d9909297fb04c3237353313
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-26 19:43:03 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happeeee :)</p>
+      <p>Start editing to see some magic chappeeee :)</p>
     </div>
   );
 }
