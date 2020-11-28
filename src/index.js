963092cd411facbb1fc1ccd2708d7b6c53a5be83
projektmati5 <mateuszprojekcik5@protonmail.com>
2020-11-28 18:48:43 +0100

    1 file modified

--- src/index.js
+++ src/index.js
@@ -47,8 +47,23 @@ return(
     );
 
 
-
-
+class Game extends React.Component{
+  render()
+  {return
+  (
+  <div className="game">
+   <div className="game-board">
+   <Board/>
+   </div>
+   </div>
+   <div className="game-info">
+   <div>{}</div>
+   <ol>{}</ol>
+   </div></di
+   );
+   
+}
+}
 
 
 
